d24645e3ffa1b3a66b80f2b05d6b4becc3e3f862
Manuel-PC <mzamalloa97@gmail.com>
2021-12-10 02:27:06 -0500

    some additions

---
+++ index/JS/nav.js
@@ -0,0 +1,3 @@
+document.querySelector(".select").textContent = "Select a Design";
+document.querySelector(".resources").textContent = "Resources";
+document.querySelector(".archives").textContent = "Archives";
